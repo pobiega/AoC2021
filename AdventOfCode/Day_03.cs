5c0dc4e4062584e230206eac8b16901c314f4c4a
Robin Rexstedt <robin.aakerman@gmail.com>
2021-12-03 11:49:59 +0100

    Day 03

---
+++ AdventOfCode/Day_03.cs
@@ -0,0 +1,110 @@
+﻿using System.Text;
+
+namespace AdventOfCode;
+
+public class Day_03 : BaseDay
+{
+    private readonly string[] _input;
+
+    public Day_03()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new(Part1(_input).ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new(Part2(_input).ToString());
+    }
+
+    public static int[] BitDiff(string[] values)
+    {
+        var count = new int[values[0].Length];
+
+        foreach (var item in values)
+        {
+            for (int i = 0; i < item.Length; i++)
+            {
+                count[i] += item[i] == '0' ? -1 : 1;
+            }
+        }
+
+        return count;
+    }
+
+    public static int ConvertBinaryStringToInt(string binaryString)
+    {
+        return Convert.ToInt32(binaryString, 2);
+    }
+
+    public static string InvertBinaryString(string binaryString)
+    {
+        var sb = new StringBuilder();
+
+        foreach (var item in binaryString)
+        {
+            if (item == '0')
+                sb.Append('1');
+            else if (item == '1')
+                sb.Append('0');
+            else
+            {
+                throw new Exception($"Invalid character in binary string: {item}");
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    public static int Part1(string[] values)
+    {
+        var bitDiff = BitDiff(values);
+
+        var gammaBuilder = new StringBuilder();
+        foreach (var item in bitDiff)
+        {
+            gammaBuilder.Append(item > 0 ? '1' : '0');
+        }
+
+        var gammaBinary = gammaBuilder.ToString();
+        var epsilonBinary = InvertBinaryString(gammaBinary);
+
+        var gamma = ConvertBinaryStringToInt(gammaBinary);
+        var epsilon = ConvertBinaryStringToInt(epsilonBinary);
+
+        return gamma * epsilon;
+    }
+
+    public static int Part2(IEnumerable<string> input)
+    {
+        var oxygenBinary = Find(input, c => c < 0);
+        var co2Binary = Find(input, c => c >= 0);
+
+        var oxygen = ConvertBinaryStringToInt(oxygenBinary);
+        var co2 = ConvertBinaryStringToInt(co2Binary);
+
+        return oxygen * co2;
+    }
+
+    public static string Find(IEnumerable<string> input, Func<int, bool> predicate)
+    {
+        var haystack = input.ToArray();
+        var needle = "";
+        var index = 0;
+
+        while (haystack.Length > 1)
+        {
+            var commonBits = BitDiff(haystack);
+
+            needle += predicate(commonBits[index]) ? '0' : '1';
+            haystack = haystack.Where(s => s.StartsWith(needle)).ToArray();
+            index++;
+        }
+
+        return haystack[0];
+    }
+}
---
+++ AdventOfCodeTests/Day03Tests.cs
@@ -0,0 +1,153 @@
+﻿using AdventOfCode;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCodeTests;
+
+public class Day03Tests
+{
+    [Theory]
+    [InlineData("00100", "00100")]
+    [InlineData("11100", "11100")]
+    [InlineData("11111", "11111")]
+    public void CanFindMostCommonBits(string input, string expected)
+    {
+        //var actual = Day_03.GetMostCommonBits(new[] { input });
+        //actual.Should().BeEquivalentTo(expected);
+    }
+
+    [Theory]
+    [InlineData("00100", "11011")]
+    [InlineData("11100", "00011")]
+    [InlineData("11111", "00000")]
+    public void CanInvertBinaryString(string input, string expected)
+    {
+        var actual = Day_03.InvertBinaryString(input);
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    [Theory]
+    [InlineData("10110", 22)]
+    public void CanConvertBinaryStringToInt(string input, int expected)
+    {
+        var actual = Day_03.ConvertBinaryStringToInt(input);
+        actual.Should().Be(expected);
+    }
+
+    [Fact]
+    public void CanCalculateGamma()
+    {
+        var input = new[]
+        {
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010"
+        };
+
+        //var actual = Day_03.GetMostCommonBits(input);
+        //actual.Should().BeEquivalentTo("10110");
+    }
+
+    [Fact]
+    public void Part1_Sample()
+    {
+        var input = new[]
+        {
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010"
+        };
+
+        var actual = Day_03.Part1(input);
+        actual.Should().Be(198);
+    }
+
+    [Fact]
+    public void FindOxygen()
+    {
+        var input = new[]
+        {
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010"
+        };
+
+        var actual = Day_03.Find(input, c => c < 0);
+
+        actual.Should().BeEquivalentTo("10111");
+    }
+
+    [Fact]
+    public void FindCO2()
+    {
+        var input = new[]
+        {
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010"
+        };
+
+        var actual = Day_03.Find(input, c => c >= 0);
+
+        actual.Should().BeEquivalentTo("01010");
+    }
+
+    [Fact]
+    public void Part2_Sample()
+    {
+        var input = new[]
+        {
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010"
+        };
+
+        var actual = Day_03.Part2(input);
+        actual.Should().Be(230);
+    }
+}
