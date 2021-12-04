3ac435ac28f212982fb5b045abbb5dcdd686be1f
Robin Rexstedt <robin.aakerman@gmail.com>
2021-12-04 13:35:13 +0100

    Day 04

---
+++ AdventOfCode/Day_04.cs
@@ -0,0 +1,194 @@
+﻿namespace AdventOfCode;
+
+public class Day_04 : BaseDay
+{
+    private readonly int[] _draw;
+    private readonly Board[] _boards;
+
+    public Day_04()
+    {
+        var lines = File.ReadAllLines(InputFilePath);
+
+        _draw = lines.First().Split(',').Select(int.Parse).ToArray();
+
+        _boards = GenerateBoards(lines.Skip(1)).ToArray();
+    }
+
+    public static IEnumerable<Board> GenerateBoards(IEnumerable<string> lines)
+    {
+        var buffer = new List<int>(Board.SIDE_LENGTH * Board.SIDE_LENGTH);
+
+        foreach (var line in lines)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                continue;
+            }
+
+            var split = line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+            buffer.AddRange(split.Select(int.Parse));
+
+            if (buffer.Count == 25)
+            {
+                yield return new Board(buffer.ToArray());
+                buffer.Clear();
+            }
+        }
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new(Part1(_draw, _boards).ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new(Part2(_draw, _boards).ToString());
+    }
+
+    public static int Part1(int[] draw, Board[] boards)
+    {
+        var markedNumbers = new HashSet<int>(draw.Length);
+
+        var first4 = draw.Take(4);
+        var remainder = draw.Skip(4);
+
+        foreach (var number in first4)
+        {
+            markedNumbers.Add(number);
+        }
+
+        foreach (var number in remainder)
+        {
+            markedNumbers.Add(number);
+
+            foreach (var board in boards)
+            {
+                if (board.CalculateWinner(markedNumbers))
+                {
+                    return Part1ScoreCalc(board, markedNumbers, number);
+                }
+            }
+        }
+
+        throw new Exception("No winner and all numbers are exhausted!");
+    }
+
+    private static int Part1ScoreCalc(Board board, HashSet<int> markedNumbers, int number)
+    {
+        var unmarkedNumbers = board.GetUnmarkedNumbers(markedNumbers);
+
+        var sum = unmarkedNumbers.Sum();
+
+        return sum * number;
+    }
+
+    public static int Part2(int[] draw, Board[] boards)
+    {
+        var markedNumbers = new HashSet<int>(draw.Length);
+
+        var first4 = draw.Take(4);
+        var remainder = draw.Skip(4);
+
+        Board mostRecentWinner = null;
+        int numberThatWon = -1;
+        int[] markedNumbersAtWin = new[] {0};
+
+        foreach (var number in first4)
+        {
+            markedNumbers.Add(number);
+        }
+
+        foreach (var number in remainder)
+        {
+            markedNumbers.Add(number);
+
+            foreach (var board in boards.Where(b => !b.HasWon))
+            {
+                if (board.CalculateWinner(markedNumbers))
+                {
+                    mostRecentWinner = board;
+                    numberThatWon = number;
+                    markedNumbersAtWin = markedNumbers.ToArray();
+                }
+            }
+        }
+
+        if(mostRecentWinner != null)
+        {
+            return Part1ScoreCalc(mostRecentWinner, markedNumbersAtWin.ToHashSet(), numberThatWon);
+        }
+
+        throw new Exception("No winner and all numbers are exhausted!");
+    }
+}
+
+public class Board
+{
+    public const int SIDE_LENGTH = 5;
+
+    private readonly int[] _numbers;
+
+    public bool HasWon { get; private set; }
+
+    public Board(int[] numbers)
+    {
+        if (numbers.Length != (SIDE_LENGTH * SIDE_LENGTH))
+        {
+            throw new Exception("Invalid board length");
+        }
+        _numbers = numbers;
+    }
+
+    public int this[int x, int y]
+    {
+        get
+        {
+            return _numbers[(y * SIDE_LENGTH) + x];
+        }
+    }
+
+    public bool CalculateWinner(HashSet<int> markedNumbers)
+    {
+        for (int y = 0; y < SIDE_LENGTH; y++)
+        {
+            if (markedNumbers.Contains(this[0, y]) &&
+                markedNumbers.Contains(this[1, y]) &&
+                markedNumbers.Contains(this[2, y]) &&
+                markedNumbers.Contains(this[3, y]) &&
+                markedNumbers.Contains(this[4, y]))
+            {
+                HasWon = true;
+                return true;
+            }
+        }
+
+        for (int x = 0; x < SIDE_LENGTH; x++)
+        {
+            if (markedNumbers.Contains(this[x, 0]) &&
+                markedNumbers.Contains(this[x, 1]) &&
+                markedNumbers.Contains(this[x, 2]) &&
+                markedNumbers.Contains(this[x, 3]) &&
+                markedNumbers.Contains(this[x, 4]))
+            {
+                HasWon = true;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public IEnumerable<int> GetUnmarkedNumbers(HashSet<int> markedNumbers)
+    {
+        for (int y = 0; y < SIDE_LENGTH; y++)
+        {
+            for (int x = 0; x < SIDE_LENGTH; x++)
+            {
+                if (!markedNumbers.Contains(this[x, y]))
+                    yield return this[x, y];
+            }
+        }
+    }
+}
---
+++ AdventOfCodeTests/Day04Tests.cs
@@ -0,0 +1,75 @@
+﻿using AdventOfCode;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace AdventOfCodeTests;
+
+public class Day04Tests
+{
+    [Fact]
+    public void BoardCanIndex()
+    {
+        var board = new Board(new[] { 0, 1, 2, 3, 4, 10, 11, 12, 13, 14, 20, 21, 22, 23, 24, 30, 31, 32, 33, 34, 40, 41, 42, 43, 44 });
+
+        board[0, 0].Should().Be(0);
+        board[0, 1].Should().Be(10);
+        board[4, 1].Should().Be(14);
+    }
+
+    [Fact]
+    public void BoardCanDetectHorizontalWins()
+    {
+        var board = new Board(new[] { 0, 1, 2, 3, 4, 10, 11, 12, 13, 14, 20, 21, 22, 23, 24, 30, 31, 32, 33, 34, 40, 41, 42, 43, 44 });
+
+        var markedNumbers = new HashSet<int> { 0, 1, 2, 3, 4 };
+
+        board.CalculateWinner(markedNumbers).Should().BeTrue();
+    }
+
+    [Fact]
+    public void BoardCanDetectVerticalWins()
+    {
+        var board = new Board(new[] { 0, 1, 2, 3, 4, 10, 11, 12, 13, 14, 20, 21, 22, 23, 24, 30, 31, 32, 33, 34, 40, 41, 42, 43, 44 });
+
+        var markedNumbers = new HashSet<int> { 0, 10, 20, 30, 40 };
+
+        board.CalculateWinner(markedNumbers).Should().BeTrue();
+    }
+
+    [Fact]
+    public void CanGenerateBoards()
+    {
+        var lines = new[]
+        {
+            //"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1",
+            "",
+            "22 13 17 11  0",
+            " 8  2 23  4 24",
+            "21  9 14 16  7",
+            " 6 10  3 18  5",
+            " 1 12 20 15 19",
+            "",
+            " 3 15  0  2 22",
+            " 9 18 13 17  5",
+            "19  8  7 25 23",
+            "20 11 10 24  4",
+            "14 21 16 12  6",
+            "",
+            "14 21 17 24  4",
+            "10 16 15  9 19",
+            "18  8 23 26 20",
+            "22 11 13  6  5",
+            " 2  0 12  3  7",
+        };
+
+        var boards = Day_04.GenerateBoards(lines).ToArray();
+
+        boards.Length.Should().Be(3);
+
+        boards[0][0, 0].Should().Be(22);
+        boards[1][0, 0].Should().Be(3);
+        boards[2][0, 0].Should().Be(14);
+    }
+}
