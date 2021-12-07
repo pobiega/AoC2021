c9d16413d3b5f37bf1ed3e88a6974010f0c66b5a
Robin Rexstedt <robin.aakerman@gmail.com>
2021-12-07 07:36:57 +0100

    Day 07

---
+++ AdventOfCode/Day_07.cs
@@ -0,0 +1,63 @@
+﻿namespace AdventOfCode;
+
+public class Day_07 : BaseDay
+{
+    private readonly int[] _crabs;
+
+    public Day_07()
+    {
+        _crabs = File.ReadAllText(InputFilePath).Split(',').Select(int.Parse).ToArray();
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new(Part1(_crabs).ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new(Part2(_crabs).ToString());
+    }
+
+    public static int Part1(int[] positions)
+    {
+        return FindBestHorizontalPosition(positions, c => c).total;
+    }
+
+    public static int Part2(int[] positions)
+    {
+        return FindBestHorizontalPosition(positions, c => SumZeroToN(c)).total;
+    }
+
+    public static (int position, int total) FindBestHorizontalPosition(int[] positions, Func<int,int> calculateFuelCost)
+    {
+        var min = positions.Min();
+        var max = positions.Max();
+
+        var lowestTotal = int.MaxValue;
+        var lowestTarget = int.MaxValue;
+
+        foreach (var target in Enumerable.Range(min, max-min))
+        {
+            var total = 0;
+
+            foreach (var crab in positions)
+            {
+                total += calculateFuelCost(Math.Abs(target - crab));
+            }
+
+            if(total < lowestTotal)
+            {
+                lowestTarget = target;
+                lowestTotal = total;
+            }
+        }
+
+        return (lowestTarget, lowestTotal);
+    }
+
+    public static int SumZeroToN(int n)
+    {
+        return n * (n + 1) / 2;
+    }
+}
---
+++ AdventOfCodeTests/Day07Tests.cs
@@ -0,0 +1,43 @@
+﻿using AdventOfCode;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AdventOfCodeTests;
+
+public class Day07Tests
+{
+    [Fact]
+    public void Part1_Samples()
+    {
+        var input = new[] { 16, 1, 2, 0, 4, 2, 7, 1, 2, 14 };
+
+        var actual = Day_07.Part1(input);
+
+        actual.Should().Be(37);
+    }
+
+    [Fact]
+    public void FindBestHorizontalPositionPart1()
+    {
+        var input = new[] { 16, 1, 2, 0, 4, 2, 7, 1, 2, 14 };
+
+        var actual = Day_07.FindBestHorizontalPosition(input, c => c);
+
+        actual.Should().Be((2, 37));
+    }
+
+    [Fact]
+    public void FindBestHorizontalPositionPart2()
+    {
+        var input = new[] { 16, 1, 2, 0, 4, 2, 7, 1, 2, 14 };
+
+        var actual = Day_07.FindBestHorizontalPosition(input, c => Day_07.SumZeroToN(c));
+
+        actual.Should().Be((5, 168));
+    }
+}
