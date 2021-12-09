6afeeec4c9d18ef2630bbd29118086caa8230e20
Robin Rexstedt <robin.aakerman@gmail.com>
2021-12-09 07:41:59 +0100

    Day 09 part 1

---
+++ AdventOfCode/Day_09.cs
@@ -0,0 +1,83 @@
+﻿namespace AdventOfCode;
+
+public class Day_09 : BaseDay
+{
+    private readonly int[][] _input;
+
+    public Day_09()
+    {
+        _input = ParseInput(File.ReadAllLines(InputFilePath));
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new(Part1(_input).ToString());
+    }
+
+    public static int[][] ParseInput(string[] input)
+    {
+        return input.Select(s => s
+                .Select(c =>
+                    int.Parse(new[] { c }))
+                .ToArray())
+            .ToArray();
+    }
+
+    public static int Part1(int[][] data)
+    {
+        var lowpoints = GetLowpoints(data);
+
+        return lowpoints.Sum(x => x.value + 1);
+    }
+
+    public static (int x, int y, int value)[] GetLowpoints(int[][] data)
+    {
+        var lowpoints = new List<(int x, int y, int value)>();
+
+        for (int y = 0; y < data.Length; y++)
+        {
+            for (int x = 0; x < data[y].Length; x++)
+            {
+                var current = data[y][x];
+
+                if (y != 0)
+                {
+                    if (current >= data[y - 1][x])
+                    {
+                        continue;
+                    }
+                }
+
+                if (y <= data.Length - 2)
+                {
+                    if (current >= data[y + 1][x])
+                    {
+                        continue;
+                    }
+                }
+                if (x != 0)
+                {
+                    if (current >= data[y][x - 1])
+                    {
+                        continue;
+                    }
+                }
+                if (x <= data[y].Length - 2)
+                {
+                    if (current >= data[y][x + 1])
+                    {
+                        continue;
+                    }
+                }
+                lowpoints.Add(new(x, y, current));
+            }
+        }
+
+        return lowpoints.ToArray();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AdventOfCodeTests/Day09Tests.cs
@@ -0,0 +1,36 @@
+﻿using AdventOfCode;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AdventOfCodeTests;
+
+public class Day09Tests
+{
+    [Fact]
+    public void Part1_Sample()
+    {
+        var input = new[]
+        {
+            "2199943210",
+            "3987894921",
+            "9856789892",
+            "8767896789",
+            "9899965678",
+        };
+
+        var data = Day_09.ParseInput(input);
+
+        data[0][0].Should().Be(2);
+        data[1][0].Should().Be(3);
+        data[4][9].Should().Be(8);
+
+        var p1 = Day_09.Part1(data);
+
+        p1.Should().Be(15);
+    }
+}
