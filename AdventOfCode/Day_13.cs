d59dff73eb139a9e1c2657773a7b3a5ffbb3b5f9
Robin Rexstedt <robin.aakerman@gmail.com>
2021-12-13 09:38:53 +0100

    Day 13 and spectre console

---
+++ AdventOfCode/Day_13.cs
@@ -0,0 +1,146 @@
+﻿using Spectre.Console;
+
+namespace AdventOfCode;
+
+public enum XY
+{
+    X,
+    Y
+}
+
+public record struct FoldLine(XY Direction, int Position);
+
+public class Day_13 : BaseDay
+{
+    private readonly Point[] _points;
+    private readonly FoldLine[] _folds;
+
+    public Day_13()
+    {
+        var (points, folds) = ParseInput(File.ReadAllLines(InputFilePath));
+
+        _points = points;
+        _folds = folds;
+    }
+
+    public static (Point[] points, FoldLine[] folds) ParseInput(string[] lines)
+    {
+        var points = new List<Point>();
+        var folds = new List<FoldLine>();
+
+        foreach (var item in lines)
+        {
+            if (string.IsNullOrWhiteSpace(item))
+            {
+                continue;
+            }
+
+            if (item.StartsWith("fold"))
+            {
+                var remainder = item.Replace("fold along ", "");
+                var values = remainder.Split('=');
+
+                folds.Add(new FoldLine(values[0] == "x" ? XY.X : XY.Y, int.Parse(values[1])));
+                continue;
+            }
+
+            var split = item.Split(',');
+            var x = int.Parse(split[0]);
+            var y = int.Parse(split[1]);
+
+            points.Add(new Point(x, y));
+
+        }
+
+        return (points.ToArray(), folds.ToArray());
+    }
+
+    public static Point[] Fold(Point[] points, XY direction, int position)
+    {
+        var result = new HashSet<Point>();
+
+        if (direction == XY.X)
+        {
+            var maxX = points.Max(p => p.X);
+
+            foreach (var point in points)
+            {
+                if (point.X < position)
+                {
+                    result.Add(point);
+                    continue;
+                }
+                else if (point.X > position)
+                {
+                    var newx = maxX - point.X;
+                    result.Add(point with { X = newx });
+                }
+            }
+        }
+        else if (direction == XY.Y)
+        {
+            var maxY = points.Max(p => p.Y);
+
+            foreach (var point in points)
+            {
+                if (point.Y < position)
+                {
+                    result.Add(point);
+                    continue;
+                }
+                else if (point.Y > position)
+                {
+                    var newY = maxY - point.Y;
+                    result.Add(point with { Y = newY });
+                }
+            }
+        }
+
+        return result.ToArray();
+    }
+
+    public static int Part1(Point[] points, FoldLine fold)
+    {
+        var actual = Fold(points, fold.Direction, fold.Position);
+
+        return actual.Length;
+    }
+
+    public static void Part2(Point[] points, FoldLine[] folds)
+    {
+        var actual = points;
+
+        foreach (var fold in folds)
+        {
+            actual = Fold(actual, fold.Direction, fold.Position);
+        }
+
+        var count = actual.Length;
+
+        var width = actual.Max(p => p.X) + 1;
+        var height = actual.Max(p => p.Y) + 1;
+
+        // replace this with a console based print..
+
+        var canvas = new Canvas(width, height);
+
+        foreach (var item in actual)
+        {
+            canvas.SetPixel(item.X, item.Y, Color.Red);
+        }
+
+        AnsiConsole.Write(canvas);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new(Part1(_points, _folds[0]).ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        Part2(_points, _folds);
+
+        return new("Printed to console.");
+    }
+}
---
+++ AdventOfCodeTests/Day13Tests.cs
@@ -0,0 +1,106 @@
+﻿using AdventOfCode;
+using Xunit;
+using FluentAssertions;
+
+namespace AdventOfCodeTests;
+
+public class Day13Tests
+{
+    [Fact]
+    public void CanParse()
+    {
+        var input = new[]
+        {
+            "0,0",
+            "0,2",
+            "",
+            "fold along x=1"
+        };
+
+        var (points, folds) = Day_13.ParseInput(input);
+
+        points.Should().HaveCount(2);
+        folds.Should().HaveCount(1);
+
+        points[0].Should().BeEquivalentTo(new Point(0, 0));
+        folds[0].Should().BeEquivalentTo(new FoldLine(XY.X, 1));
+    }
+
+    [Fact]
+    public void CanFoldX()
+    {
+        var input = new[]
+        {
+            "4,0",
+            "3,0",
+            "4,1",
+            "",
+            "fold along x=2"
+        };
+
+        var (points, folds) = Day_13.ParseInput(input);
+
+        var actual = Day_13.Fold(points, XY.X, 2);
+
+        actual.Should().HaveCount(3);
+        actual.Should().ContainEquivalentOf(new Point(0, 0));
+        actual.Should().ContainEquivalentOf(new Point(1, 0));
+        actual.Should().ContainEquivalentOf(new Point(0, 1));
+    }
+
+    [Fact]
+    public void CanFoldY()
+    {
+        var input = new[]
+        {
+            "0,4",
+            "0,3",
+            "1,4",
+            "",
+            "fold along y=2"
+        };
+
+        var (points, folds) = Day_13.ParseInput(input);
+
+        var actual = Day_13.Fold(points, XY.Y, 2);
+
+        actual.Should().HaveCount(3);
+        actual.Should().ContainEquivalentOf(new Point(0, 0));
+        actual.Should().ContainEquivalentOf(new Point(1, 0));
+        actual.Should().ContainEquivalentOf(new Point(0, 1));
+    }
+
+    [Fact]
+    public static void Part1_Sample()
+    {
+        var input = new[]
+        {
+            "6,10",
+            "0,14",
+            "9,10",
+            "0,3",
+            "10,4",
+            "4,11",
+            "6,0",
+            "6,12",
+            "4,1",
+            "0,13",
+            "10,12",
+            "3,4",
+            "3,0",
+            "8,4",
+            "1,10",
+            "2,14",
+            "8,10",
+            "9,0",
+            "",
+            "fold along y=7",
+        };
+
+        var (points, folds) = Day_13.ParseInput(input);
+
+        var actual = Day_13.Part1(points, folds[0]);
+
+        actual.Should().Be(17);
+    }
+}
